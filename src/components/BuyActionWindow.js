6d6f6afe3ec5aef05128b232151958c87958f583
SreeyaKadari <sreeyakadari2024@gmail.com>
2025-08-20 19:13:03 -0500

    updated code

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -9,7 +9,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodhabackend-oki2.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
